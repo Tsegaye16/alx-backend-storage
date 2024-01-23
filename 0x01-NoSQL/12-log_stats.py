64716e01896fcaf57e0aa4128954cc3d07d9a8c3
Tsegaye16 <abewatsegaye16@gmail.com>
2024-01-23 06:47:57 +0000

    second day of back-end storage

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_logs = client.logs.nginx
-    """ get number of documents in collection """
+    # get number of documents in collection
     docs_num = nginx_logs.count_documents({})
     get_num = nginx_logs.count_documents({'method': 'GET'})
     post_num = nginx_logs.count_documents({'method': 'POST'})
